7ef535c66eef9fc3e98d378d9fd0b3ec9d55c8ba
哈哈哈 <hahaha@hahaah.com>
2024-12-04 13:48:53 +0800

    第一次提交

--- src/main/java/org/example/myspring/MySpringApplication.java
+++ src/main/java/org/example/myspring/MySpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyspringApplication {
+public class MySpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyspringApplication.class, args);
+        SpringApplication.run(MySpringApplication.class, args);
     }
 
 }
