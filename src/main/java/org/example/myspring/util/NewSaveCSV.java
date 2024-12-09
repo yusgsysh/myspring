7f5764d4e6687454cbc0c0683c879dcc7afdf4ef
哈哈哈 <hahaha@hahaha.com>
2024-12-09 12:59:37 +0800

    重构save/getcsv

---
+++ src/main/java/org/example/myspring/util/NewSaveCSV.java
@@ -0,0 +1,28 @@
+package org.example.myspring.util;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class NewSaveCSV {
+    public static void saveCSV(String userToken,Map<String, String> hashMap, String path) {
+        String csvFilePath = path + "/" +userToken + ".csv";
+        try (CSVPrinter printer = new CSVPrinter(new FileWriter(csvFilePath,true), CSVFormat.DEFAULT)) {
+            // 检查文件是否为空，如果是，则写入表头
+            if (new java.io.File(csvFilePath).length() == 0) {
+                printer.printRecord("Key", "Value");
+            }
+            // 写入数据
+            for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+                printer.printRecord(entry.getKey(), entry.getValue());
+            }
+            System.out.println("Success wrote");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
