91d01c7bd6d0d26410f5ae7dd07a906908dadd70
hahaha <hahaha@hahaha.com>
2024-12-10 09:44:47 +0800

    csv文件自动更新去重

--- src/main/java/org/example/myspring/util/SaveCSV.java
+++ src/main/java/org/example/myspring/util/SaveCSV.java
@@ -5,6 +5,7 @@ import org.apache.commons.csv.CSVPrinter;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.example.myspring.util.GetCSV.getCSV;
@@ -12,14 +13,15 @@ import static org.example.myspring.util.GetCSV.getCSV;
 public class SaveCSV {
     public static void saveCSV(String userToken, Map<String, String> hashMap, String path) {
         String csvFilePath = path + "/" + userToken + ".csv";
-        hashMap.putAll(getCSV(userToken, path));
+        Map<String, String> inputMap = getCSV(userToken, path);
+        inputMap.putAll(hashMap);
         try (CSVPrinter printer = new CSVPrinter(new FileWriter(csvFilePath), CSVFormat.DEFAULT)) {
             // 检查文件是否为空，如果是，则写入表头
             if (new java.io.File(csvFilePath).length() == 0) {
                 printer.printRecord("Key", "Value");
             }
             // 写入数据
-            for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            for (Map.Entry<String, String> entry : inputMap.entrySet()) {
                 printer.printRecord(entry.getKey(), entry.getValue());
             }
             System.out.println("Success wrote");
